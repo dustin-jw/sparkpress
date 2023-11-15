334cb81e29c6d9049401e36d3bcadf30b393484d
dustin-jw <dustin@heysparkbox.com>
2023-11-15 09:36:44 -0700

    feat: simplify header and footer

--- src/themes/sparkpress/patterns/footer-default.php
+++ src/themes/sparkpress/patterns/footer-default.php
@@ -8,19 +8,10 @@
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
-		<!-- wp:site-title {"level":0} /-->
-		<!-- wp:paragraph {"align":"right"} -->
-		<p class="has-text-align-right">
-		<?php
-		printf(
-			/* Translators: WordPress link. */
-			esc_html__( 'Proudly powered by %s', 'sparkpress' ),
-			'<a href="' . esc_url( __( 'https://wordpress.org', 'sparkpress' ) ) . '" rel="nofollow">WordPress</a>'
-		)
-		?>
-		</p>
+	<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:paragraph -->
+		<p>&copy; <?php echo esc_html( getdate()['year'] ); ?> | <?php echo esc_html( get_bloginfo( 'name' ) ); ?></p>
 		<!-- /wp:paragraph -->
 	</div>
 	<!-- /wp:group -->
