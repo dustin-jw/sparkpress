afc85949941d3b99feb533273fb73baf018a4e5f
dustin-jw <dustin@heysparkbox.com>
2023-11-15 11:44:20 -0700

    feat: provide fallback for no search results

--- src/themes/sparkpress/patterns/hidden-no-results.php
+++ src/themes/sparkpress/patterns/hidden-no-results.php
@@ -5,10 +5,14 @@
  * Inserter: no
  */
 ?>
-<!-- wp:paragraph -->
-<p>
-<?php echo esc_html_x( "Couldn't find what you were looking for? Please try again with some different keywords.", 'Message explaining that there are no results returned from a search', 'sparkpress' ); ?>
-</p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:paragraph -->
+	<p>
+	<?php echo esc_html_x( "Couldn't find what you were looking for? Please try again with some different keywords.", 'Message explaining that there are no results returned from a search', 'sparkpress' ); ?>
+	</p>
+	<!-- /wp:paragraph -->
 
-<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'sparkpress' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'sparkpress' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'sparkpress' ); ?>","buttonUseIcon":true} /-->
+	<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'sparkpress' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'sparkpress' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'sparkpress' ); ?>","buttonUseIcon":true} /-->
+</div>
+<!-- /wp:group -->
