2533b8ebf7753f3e10cbedd6f5d7e5df05af775f
dustin-jw <dustin@heysparkbox.com>
2023-11-15 12:21:05 -0700

    feat: simplify post meta

--- src/themes/sparkpress/patterns/post-meta.php
+++ src/themes/sparkpress/patterns/post-meta.php
@@ -7,70 +7,24 @@
  * Block Types: core/template-part/post-meta
  */
 ?>
-<!-- wp:spacer {"height":"0"} -->
-<div style="height:0" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70)">
-	<!-- wp:separator {"opacity":"css","align":"wide","className":"is-style-wide"} -->
-	<hr class="wp-block-separator alignwide has-css-opacity is-style-wide"/>
-	<!-- /wp:separator -->
-
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"fontSize":"small"} -->
-	<div class="wp-block-columns alignwide has-small-font-size" style="margin-top:var(--wp--preset--spacing--30)">
-		<!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:group {"style":{"spacing":{"blockGap":"0.5ch"}},"layout":{"type":"flex"}} -->
-			<div class="wp-block-group">
-				<!-- wp:paragraph -->
-				<p>
-					<?php echo esc_html_x( 'Posted', 'Verb to explain the publication status of a post', 'sparkpress' ); ?>
-				</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:post-date /-->
-
-				<!-- wp:paragraph -->
-				<p>
-					<?php echo esc_html_x( 'in', 'Preposition to show the relationship between the post and its categories', 'sparkpress' ); ?>
-				</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:post-terms {"term":"category"} /-->
-			</div>
-			<!-- /wp:group -->
-
-			<!-- wp:group {"style":{"spacing":{"blockGap":"0.5ch"}},"layout":{"type":"flex"}} -->
-			<div class="wp-block-group">
-				<!-- wp:paragraph -->
-				<p>
-					<?php echo esc_html_x( 'by', 'Preposition to show the relationship between the post and its author', 'sparkpress' ); ?>
-				</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:post-author {"showAvatar":false} /-->
-			</div>
-			<!-- /wp:group -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"fontSize":"small"} -->
+		<div class="wp-block-group has-small-font-size">
+			<!-- wp:post-date /-->
+			<!-- wp:post-author-name {"isLink":true} /-->
 		</div>
-		<!-- /wp:column -->
+		<!-- /wp:group -->
 
-		<!-- wp:column {"style":{"spacing":{"blockGap":"0px"}}} -->
-		<div class="wp-block-column">
-			<!-- wp:group {"style":{"spacing":{"blockGap":"0.5ch"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-			<div class="wp-block-group">
-				<!-- wp:paragraph -->
-				<p>
-					<?php echo esc_html_x( 'Tags:', 'Label for a list of post tags', 'sparkpress' ); ?>
-				</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:post-terms {"term":"post_tag"} /-->
-			</div>
-			<!-- /wp:group -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0.5rem"}},"fontSize":"small"} -->
+		<div class="wp-block-group has-small-font-size">
+			<!-- wp:post-terms {"term":"category","textAlign":"right"} /-->
+			<!-- wp:post-terms {"term":"post_tag","textAlign":"right"} /-->
 		</div>
-		<!-- /wp:column -->
+		<!-- /wp:group -->
 	</div>
-	<!-- /wp:columns -->
+	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
