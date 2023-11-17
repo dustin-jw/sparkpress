095658224706e805b7443aa6369409585e6974f0
dustin-jw <dustin@heysparkbox.com>
2023-11-17 11:02:16 -0700

    feat: enqueue theme styles

---
+++ src/themes/sparkpress/functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Theme functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ */
+
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
+/**
+ * Enqueue styles for the theme.
+ */
+function sparkpress_theme_styles() {
+	wp_enqueue_style( 'sparkpress-theme', get_template_directory_uri() . '/sparkpress-theme.css', array(), _S_VERSION );
+}
+add_action( 'wp_enqueue_scripts', 'sparkpress_theme_styles' );
