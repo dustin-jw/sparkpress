85acd636906c0f87c9b7de33875d0419373bd38e
dustin-jw <dustin@heysparkbox.com>
2023-11-17 11:36:12 -0700

    feat: initialize Timber for theme

--- src/themes/sparkpress/functions.php
+++ src/themes/sparkpress/functions.php
@@ -5,6 +5,28 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
+/**
+ * Twig / Timber templating.
+ */
+use Twig\Extra\String\StringExtension;
+use Twig\Extra\Html\HtmlExtension;
+Timber\Timber::init();
+
+/**
+ * Adds Twig extensions to the Twig environment to enhance functionality:
+ * Adds StringExtension (string-extra).
+ * Adds HtmlExtension (html-extra).
+ *
+ * @param \Twig\Environment $twig - The Twig environment to which extensions are added.
+ * @return \Twig\Environment The Twig environment with added extensions.
+ */
+function add_to_twig( $twig ) {
+	$twig->addExtension( new StringExtension() );
+	$twig->addExtension( new HtmlExtension() );
+	return $twig;
+}
+add_filter( 'timber/twig', 'add_to_twig' );
+
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
