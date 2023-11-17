9f6100b1f042a4882976e72e1d0736d1e341e354
dustin-jw <dustin@heysparkbox.com>
2023-11-17 11:44:28 -0700

    refactor: use twig for rendering no results pattern

--- src/themes/sparkpress/patterns/hidden-no-results.php
+++ src/themes/sparkpress/patterns/hidden-no-results.php
@@ -4,15 +4,10 @@
  * Slug: sparkpress/hidden-no-results-content
  * Inserter: no
  */
-?>
-<!-- wp:group {"align":"wide"} -->
-<div class="wp-block-group alignwide">
-	<!-- wp:paragraph -->
-	<p>
-	<?php echo esc_html_x( "Couldn't find what you were looking for? Please try again with some different keywords.", 'Message explaining that there are no results returned from a search', 'sparkpress' ); ?>
-	</p>
-	<!-- /wp:paragraph -->
 
-	<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'sparkpress' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'sparkpress' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'sparkpress' ); ?>","buttonUseIcon":true} /-->
-</div>
-<!-- /wp:group -->
+$context = Timber::context();
+$context['message'] = esc_html_x( "Couldn't find what you were looking for? Please try again with some different keywords.", 'Message explaining that there are no results returned from a search', 'sparkpress' );
+$context['search_label'] = esc_attr_x( 'Search', 'label', 'sparkpress' );
+$context['search_placeholder'] = esc_attr_x( 'Search...', 'placeholder for search field', 'sparkpress' );
+
+Timber::render( 'patterns/no-results.twig', $context );
